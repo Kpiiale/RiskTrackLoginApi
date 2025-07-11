85bcda0d7ba5763dd8987db99b41a502f3f6e398
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-11 00:07:44 -0500

    Add User Model

---
+++ RiskTrackLoginApi/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RiskTrackLoginApi.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        [MaxLength(100)]
+        public string? Username { get; set; }
+
+        [MaxLength(100)]
+        public string? Email { get; set; }
+
+        [MaxLength(100)]
+        public string? Password { get; set; }
+
+        [MaxLength(50)]
+        public string? Role { get; set; }
+ 
+    }
+}
