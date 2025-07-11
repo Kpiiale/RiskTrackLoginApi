5651ceb4428bb23153ff6a7b85ac97d1e51c6cea
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-11 00:21:27 -0500

    Add AppdbContext

---
+++ RiskTrackLoginApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using RiskTrackLoginApi.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiskTrackLoginApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("Users"); 
+                entity.HasIndex(e => e.Email).IsUnique();
+            });
+        }
+    }
+}
